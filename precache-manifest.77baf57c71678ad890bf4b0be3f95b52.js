c8d403f5d069bfa92b0f0cc20d7086befcaac292
KarolinaCeteraWojcik <karolina.cetera.wojcik@gmail.com>
2020-02-29 22:04:35 +0100

    Updates

---
+++ precache-manifest.77baf57c71678ad890bf4b0be3f95b52.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166b53049abb68d6fef0e1968c65bb60",
+    "url": "/TinDog/index.html"
+  },
+  {
+    "revision": "ff59039d823f8ebd84ee",
+    "url": "/TinDog/static/css/2.780a4f39.chunk.css"
+  },
+  {
+    "revision": "20e6c130696c0df671f3",
+    "url": "/TinDog/static/css/main.3ac1ce7d.chunk.css"
+  },
+  {
+    "revision": "ff59039d823f8ebd84ee",
+    "url": "/TinDog/static/js/2.a41d8051.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/TinDog/static/js/2.a41d8051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e6c130696c0df671f3",
+    "url": "/TinDog/static/js/main.2b43fa75.chunk.js"
+  },
+  {
+    "revision": "390b32f30f975271d773",
+    "url": "/TinDog/static/js/runtime-main.b2041258.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TinDog/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TinDog/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TinDog/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TinDog/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TinDog/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1ca4ec8d1aa07b80de7c83ae258ed1c5",
+    "url": "/TinDog/static/media/paw.1ca4ec8d.svg"
+  }
+]);
